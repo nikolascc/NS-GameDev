d94595e62b42d1e57c16b8ce4093a419ee8e415e
Nikolas C <nikolascc@hotmail.com>
2017-06-01 13:25:19 -0700

    Added project to github

---
+++ Tower Defense Python/towerdefense.py
@@ -0,0 +1,45 @@
+import pygame
+import time
+import random
+
+# initialize screen & colors
+pygame.init()
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,200,0)
+gameDisplay = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption('Tower Defense')
+
+# set clock & speed
+clock = pygame.time.Clock()
+clock.tick(60)
+
+#this line can load pngs into the game
+#block = pygame.image.load('otherbit.png')
+
+def handle_keys():
+	# This function handles all user input
+	key = pygame.key.get_pressed()
+
+def main_menu():
+	# main menu function
+	
+	# fill background
+	background = pygame.Surface(gameDisplay.get_size())
+	background = background.convert()
+	background.fill(white)
+	
+	##### EVENT LOOP #####
+	while 1:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit()
+			pygame.display.update()
+			gameDisplay.blit(background,(0,0))
+			handle_keys()
+
+# call functions here
+main_menu()
